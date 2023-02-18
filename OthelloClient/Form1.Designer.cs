f256ae863017061e471f65a8b31e8c073ab8dc2f
Michele Mastroianni <ioxdue@gmail.com>
2023-02-18 17:19:38 +0100

    Windows Form Client

---
+++ OthelloClient/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace OthelloClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.RichTextBox();
+            this.panelBoard = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 102);
+            this.button1.Margin = new System.Windows.Forms.Padding(7, 8, 7, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(188, 63);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Join\r\n";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button_Join_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 33);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(188, 58);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Connect";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button_Connect_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Font = new System.Drawing.Font("Segoe UI", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox.Location = new System.Drawing.Point(12, 620);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(838, 74);
+            this.textBox.TabIndex = 4;
+            this.textBox.Text = "";
+            // 
+            // panelBoard
+            // 
+            this.panelBoard.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelBoard.Location = new System.Drawing.Point(210, 33);
+            this.panelBoard.Name = "panelBoard";
+            this.panelBoard.Size = new System.Drawing.Size(640, 544);
+            this.panelBoard.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 41F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(877, 730);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.panelBoard);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(7, 8, 7, 8);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button3;
+        private RichTextBox textBox;
+        private Panel panelBoard;
+    }
+}
---
+++ OthelloClient/Form1.cs
@@ -0,0 +1,290 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Dynamic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace OthelloClient
+{
+    public partial class Form1 : Form
+    {
+        readonly IPAddress host = IPAddress.Parse("127.0.0.1");
+        private readonly int port = 8080;
+        private StreamWriter? writer;
+        private StreamReader? reader;
+        private TcpClient? client;
+        private NetworkStream? netstream;
+        private readonly int timeout = 5000;
+        private string player = "";
+        private char[][] board;
+        Panel[,] panelMatrix;
+        private int numRows = 8;
+        private int numCols = 8;
+
+        public Form1()
+        {
+            // Creare la matrice di pannelli
+            panelMatrix = new Panel[numRows, numCols];
+            this.board = new char[numRows][];
+            for (int i = 0; i < numCols; i++)
+                this.board[i] = new char[numCols];
+            InitializeComponent();
+        }
+
+        private void Connect()
+        {
+            try
+            {
+                textBox.Clear();
+
+                client = new TcpClient();
+
+                client.ConnectAsync(host, port)
+                    .ContinueWith(task =>
+                    {
+                        netstream = client.GetStream();
+                        writer = new StreamWriter(netstream);
+                        reader = new StreamReader(netstream);
+
+                        writer.AutoFlush = true;
+                        netstream.ReadTimeout = timeout;
+                        textBox.AppendText("Connesso!");
+                    });
+            }
+            catch (Exception a)
+            {
+                textBox.AppendText($"Connect Exception: {a.Message}");
+            }
+        }
+
+        private void DrawBoard()
+        {
+            //DRAW BOARD
+
+
+            // Creare e inizializzare ogni pannello nella matrice
+            for (int row = 0; row < numRows; row++)
+            {
+                for (int col = 0; col < numCols; col++)
+                {                        
+                    if (board[row][col] == 'W')
+                        panelMatrix[numRows - 1 - row, numCols - 1 - col].BackColor = Color.AntiqueWhite;
+                    else if (board[row][col] == 'B')
+                        panelMatrix[numRows - 1 - row, numCols - 1 - col].BackColor = Color.Black;
+                    else
+                        panelMatrix[numRows - 1 - row, numCols - 1 - col].BackColor = Color.Green;
+                }
+            }
+        }
+
+        private void Turn(dynamic res)
+        {
+            player = res.currentPlayer;
+            JArray boardArray = (JArray)res.board;
+            board = boardArray.Select(x => x.Select(y => ((string)y)[0]).ToArray()).ToArray();
+            DrawBoard();
+        }
+
+        private async void WaitMyTurn(string p)
+        {
+            try
+            {
+                dynamic myObject = new ExpandoObject();
+                myObject.type = "wait_my_turn";
+                myObject.player = p;
+
+                string json = JsonConvert.SerializeObject(myObject);
+
+
+                await writer!.WriteLineAsync(json)
+                    .ContinueWith(task =>
+                    {
+                        Read();
+                    });
+
+            }
+            catch (Exception e)
+            {
+                textBox.AppendText($"WaitMyTurn Exception: {e.Message}");
+            }
+            finally
+            {
+                //
+            }
+        }  
+
+        private void ValidMove(dynamic res)
+        {
+            string currentPlayer = res.currentPlayer;
+
+            JArray boardArray = (JArray)res.board;
+            board = boardArray.Select(x => x.Select(y => ((string)y)[0]).ToArray()).ToArray();
+
+            if(currentPlayer == player)
+            {
+                WaitMyTurn(player);
+            }
+
+            DrawBoard();
+        }
+
+        private async void Read()
+        {
+            try
+            {
+                // Prepara il buffer per leggere i dati dalla socket
+                byte[] buffer = new byte[1024];
+                StringBuilder data = new StringBuilder();
+                textBox.Clear();
+
+
+                int bytes = 0;
+                do
+                {
+                    bytes = await netstream.ReadAsync(buffer, 0, buffer.Length);
+                    data.Append(Encoding.ASCII.GetString(buffer, 0, bytes));
+                } while (netstream.DataAvailable);
+                textBox.AppendText(data.ToString());
+                //Console.WriteLine($"from Server receive: {data}");
+                var res = JsonConvert.DeserializeObject<dynamic>(data.ToString());
+
+                string type = res!.type;
+
+                switch ( type)
+                {
+                    case "your_turn":
+                        Turn(res);
+                        break;
+                    case "valid_move":
+                        ValidMove(res);
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+            catch (Exception e)
+            {
+                textBox.AppendText($"Read Exception: {e.Message}");
+            }
+            finally
+            {
+                //
+            }
+        }
+
+        private async void MoveXY(int x, int y)
+        {
+            try
+            {
+                dynamic myObject = new ExpandoObject();
+                myObject.type = "move";
+                myObject.x = x;
+                myObject.y = y;
+                myObject.player = player;
+
+                string json = JsonConvert.SerializeObject(myObject);
+
+                await writer!.WriteLineAsync(json)
+                    .ContinueWith(task =>
+                    {
+                        Read();
+                    });
+            }
+            catch (Exception e)
+            {
+                textBox.AppendText($"Move Exception: {e.Message}");
+            }
+            finally
+            {
+                //
+            }
+
+        }
+
+        private async void Join()
+        {
+            try
+            {
+                dynamic myObject = new ExpandoObject();
+                myObject.type = "join";
+
+                string json = JsonConvert.SerializeObject(myObject);
+
+                await writer!.WriteLineAsync(json)
+                    .ContinueWith(task =>
+                    {
+                        Read();
+                        textBox.AppendText("Joined!");
+                    });
+
+            }
+            catch(Exception e)
+            {
+                textBox.AppendText($"Join Exception: {e.Message}");
+            }
+            finally
+            {
+                //
+            }
+        }
+
+        private void Button_Join_Click(object sender, EventArgs e)
+        {
+            Join();
+
+        }
+
+        private void Button_Connect_Click(object sender, EventArgs e)
+        {
+            Connect();
+        }
+
+        private void Board_Click(object sender, EventArgs e)
+        {
+            string move = (string)((Panel)sender).Tag;
+            //Console.WriteLine(move);
+            int x = Int32.Parse(move.Split(",")[0]);
+            int y = Int32.Parse(move.Split(",")[1]);
+            MoveXY(x, y);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            int h = panelBoard.Size.Height / numRows;
+            int w = panelBoard.Size.Width / numCols;
+
+            // Creare e inizializzare ogni pannello nella matrice
+            for (int row = 0; row < numRows; row++)
+            {
+                for (int col = 0; col < numCols; col++)
+                {
+                    panelMatrix[row, col] = new Panel();
+                    panelMatrix[row, col].Tag = (numCols-col-1) + "," + (numRows-row-1);
+                    panelMatrix[row, col].Name = "panel_" + row.ToString() + "_" + col.ToString();
+                    panelMatrix[row, col].BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+                    panelMatrix[row, col].AutoSize = false;
+                    panelMatrix[row, col].Size = new System.Drawing.Size(w, h);
+                    panelMatrix[row, col].Location = new System.Drawing.Point(0, 0);
+                    if (col == 0)
+                    {
+                        panelMatrix[row, col].Dock = DockStyle.Top;
+                        panelBoard.Controls.Add(panelMatrix[row, col]);
+                    }
+                    else
+                    {
+                        panelMatrix[row, col].Dock = DockStyle.Left;
+                        panelMatrix[row, 0].Controls.Add(panelMatrix[row, col]);
+                    }
+                    // Aggiungere il pannello al form
+                    panelMatrix[row, col].Click += new System.EventHandler(this.Board_Click);
+                }
+            }
+            DrawBoard();
+        }
+
+
+    }
+}
